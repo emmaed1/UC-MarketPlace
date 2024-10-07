d77ca0089cb510dd957712c507196ae001a6a4a3
Elsy14 <prempeea@mail.uc.edu>
2024-10-07 14:21:24 -0400

    added pages folder

---
+++ uc-marketplace/pages/products/Productspage.js
@@ -0,0 +1,58 @@
+import "./Product.css";
+
+export default function Product() {
+  return (
+    <body>
+    <header class="sc-block sc-block--position-default-relative sc-header sc-header--v1 sc-header--vertical" 
+            data-block-id="cm1vm710c0fzlujemj2e84pn6" data-component-id="block" data-plugin-header>
+        
+        <div class="sc-header__mobile-header--show">
+            <div class="sc-header__mobile-header-icon">
+                <svg class="sc-svg-icon sc-header__mobile-header-svg">
+                    <use xlink:href="#svg-icon-mobile-hamburger"></use>
+                </svg>
+            </div>
+        </div>
+ 
+        <div class="sc-header__center-wrap sc-header__center-wrap--v1">
+            <div class="sc-header__logo">
+                <a class="sc-header__logo-link" href="/">
+                    <picture class="sc-picture sc-header__picture">
+                        <source srcset="/images/logo.webp" type="image/webp">
+                        <img src="/images/logo.png" alt="logo" class="sc-header__logo-img" title="Logo Title">
+                    </picture>
+                </a>
+            </div>
+ 
+            <div class="sc-header__menu-main-wrap">
+                <ul class="sc-header__menu-main">
+                    <li class="sc-header__menu-main-item">
+                        <a class="sc-header__menu-main-link" href="#">Menu Item 1</a>
+                    </li>
+                    <li class="sc-header__menu-main-item">
+                        <a class="sc-header__menu-main-link" href="#">Menu Item 2</a>
+                    </li>
+                    <li class="sc-header__menu-main-item">
+                        <a class="sc-header__menu-main-link" href="#">Menu Item 3</a>
+                    </li>
+                    <li class="sc-header__menu-main-item">
+                        <a class="sc-header__menu-main-link" href="#">Menu Item 4</a>
+                    </li>
+                </ul>
+            </div>
+ 
+            <div class="sc-header__menu-secondary-wrap">
+                <ul class="sc-header__menu-secondary">
+                    <li class="sc-header__menu-secondary-item">
+                        <a class="sc-header__menu-secondary-link" href="#">Secondary Menu Item 1</a>
+                    </li>
+                    <li class="sc-header__menu-secondary-item">
+                        <a class="sc-header__menu-secondary-link" href="#">Secondary Menu Item 2</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</body>
+  );
+}
