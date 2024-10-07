8a22542a016156f720e8dc8273ab446e7044c69c
Emma Brantley <brantled@mail.uc.edu>
2024-10-07 19:28:26 -0400

    stuff

--- uc-marketplace/src/App.js
+++ uc-marketplace/src/App.js
@@ -15,15 +15,6 @@ function App() {
       <Context.Provider value={userInfo}>
         <Router />
       </Context.Provider>
-      <div className="content">
-        
-          
-        {/* This is where images or welcome part is */}
-        <div className="reviews">
-          {/* This space is for reviews/testimonials */}
-        </div>
-        <div className="faq">{/* This space is for FAQ */}</div>
-      </div>
     </div>
   );
 }
