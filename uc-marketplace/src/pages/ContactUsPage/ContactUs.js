c1a98b09d27318f7ee202ca241f4bec75a06af53
Emma Brantley <brantled@mail.uc.edu>
2024-10-21 18:46:37 -0400

    initial stuff

--- uc-marketplace/src/pages/ContactUsPage/ContactUs.js
+++ uc-marketplace/src/pages/ContactUsPage/ContactUs.js
@@ -1,3 +1,29 @@
-<div>
-    <p>Contact Us</p>
+import './ContactUs.css'
+
+export default function AccountView() {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>Contact Us</h1>
+      </div>
+
+      <div className="inputs">
+          <div className="input">
+            <input type="text" placeholder="Name"></input>
+          </div>
+      </div>
+
+      <div className="inputs">
+        <div className="input">
+          <input type="email" placeholder="Email"></input>
+        </div>
+      </div>
+
+      <div className="inputs">
+        <div className="input">
+          <input type="text" placeholder="Description"></input>
+        </div>
+      </div>
 </div>
+  );
+}
