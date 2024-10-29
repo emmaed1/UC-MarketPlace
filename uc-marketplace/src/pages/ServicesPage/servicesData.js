580c988db17c530676406d77b33877bc867087f4
Emma Brantley <brantled@mail.uc.edu>
2024-10-29 17:50:02 -0400

    consistency

---
+++ uc-marketplace/src/pages/ServicesPage/servicesData.js
@@ -0,0 +1,68 @@
+const servicesData = [
+    {
+        id: 1,
+        // img: "",
+        rating: "★★★★",
+        title: "This is a test",
+        price: 54500,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        // img: "",
+        rating: "★★★★★",
+        title: "test2",
+        price: 31000,
+        quantity: 1,
+    },
+    {
+        id: 3,
+        // img: "",
+        rating: "★★★★",
+        title: "test3",
+        price: 84007,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        // img: "",
+        rating: "★★★★★",
+        title: "test4",
+        price: 53570,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        // img: "",
+        rating: "★★★★",
+        title: "test5",
+        price: 147990,
+        quantity: 1,
+    },
+    {
+        id: 6,
+        // img: "",
+        rating: "★★★★★",
+        title: "test6",
+        price: 54500,
+        quantity: 1,
+    },
+    {
+        id: 7,
+        // img: "",
+        rating: "★★★★",
+        title: "test7",
+        price: 24990,
+        quantity: 1,
+    },
+    {
+        id: 8,
+        // img: "",
+        rating: "★★★★",
+        title: "test8",
+        price: 54500,
+        quantity: 1,
+    }
+];
+
+export default servicesData;
