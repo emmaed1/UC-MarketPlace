e4b34b7da8a653fd8d8a4e152fd6bbe4219956b9
Parker-Blaine <parkebs@mail.uc.edu>
2025-01-23 12:13:21 -0500

    UpdatedProductsPageForMessage

--- uc-marketplace/src/pages/Products/products.js
+++ uc-marketplace/src/pages/Products/products.js
@@ -1,17 +1,22 @@
 import "./products.css";
 import ProductsCard from "./productsCard";
 import { useEffect, useState } from "react";
+import MessageForm from "../../components/MessageForm"; // Import MessageForm component
 
-const Products = () => {
-  // const products = productsData
+const Products = ({ userId }) => {
   useEffect(() => {
-      fetch('http://localhost:3001/products', {method: "GET"})
-        .then(res => res.json())
-        .then(data => getProducts(data))
-        .catch(err => {console.log("Error getting products", err)});
+    fetch('http://localhost:3001/products', { method: "GET" })
+      .then(res => res.json())
+      .then(data => getProducts(data))
+      .catch(err => { console.log("Error getting products", err) });
   }, []);
 
   const [products, getProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  const handleChatClick = (product) => {
+    setSelectedProduct(product);
+  };
 
   return (
     <>
@@ -28,11 +33,21 @@ const Products = () => {
           <input type="text" id="search" placeholder="Search for products..." />
         </div>
         <div className="products-content">
-          {products.map((item) => (
-            <ProductsCard key={item.productId}{...item} />
+          {products.map((product) => (
+            <div key={product.productId}>
+              <ProductsCard {...product} />
+              <button onClick={() => handleChatClick(product)}>Chat with Seller</button>
+            </div>
           ))}
         </div>
       </div>
+      {selectedProduct && (
+        <MessageForm
+          senderId={userId}
+          receiverId={selectedProduct.sellerId}
+          productId={selectedProduct.productId}
+        />
+      )}
     </>
   );
 };
