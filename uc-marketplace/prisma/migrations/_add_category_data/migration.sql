5520f0f4c440d33d1a495a8115e0a79ea1f332e2
Seth Okai <sethokai5@gmail.com>
2025-02-20 16:48:11 -0500

    feat: add initial category data migration

---
+++ uc-marketplace/prisma/migrations/_add_category_data/migration.sql
@@ -0,0 +1,23 @@
+-- Insert Product Categories
+INSERT INTO "ProductCategory" (name) VALUES
+  ('Academic Materials'),
+  ('Home Essentials'),
+  ('Clothing'),
+  ('Accesories'),
+  ('Technology & Electronics'),
+  ('Food & Beverage'),
+  ('Entertainment'),
+  ('Collectibles'),
+  ('Miscellaneous');
+
+-- Insert Service Categories  
+INSERT INTO "ServiceCategory" (name) VALUES
+  ('Academic Help'),
+  ('Technology Support'),
+  ('Photography & Videography'),
+  ('Beauty & Personal Care'),
+  ('Automotive Services'),
+  ('Creative Work'),
+  ('Pet Services'),
+  ('Entertainment & Event Planning'),
+  ('Miscellaneous'); 
